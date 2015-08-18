9e7314e4cdb8a8fc51d039a90834074bccc637f8
Lars T Hansen <lth@acm.org>
2015-08-18 10:34:45 +0200

    Update to ecmarkup 1.7

--- format.sh
+++ format.sh
@@ -1,19 +1,21 @@
+#!/bin/bash
+#
 # This script will create tc39/shmem.html from the spec sources.
 #
 # First, you need to:
 #
 # Install io.js, because ecmarkup needs it:
 #   Download installer from iojs.org and run it
-#   Make sure that the "node" application that's in your path points to /usr/local/bin/iojs
 #
 # Install ecmarkup:
 #   sudo npm install -g ecmarkup
+#
+# ecmarkup 1.6 or greater is required.
 
-# The ecmarkup I have does not handle some formatting characters that
-# ecmarkdown should be able to handle.  So process those first.
+# Maybe io.js is not the default node, work around that if we can.
 
-node preprocess.js tc39/spec.html temp.html
-
-# Then run the markup.
-
-ecmarkup temp.html tc39/shmem.html
+if [ $(which iojs) != "" ]; then
+    PATH=$(dirname $(which iojs)):$PATH ecmarkup tc39/spec.html tc39/shmem.html
+else
+    ecmarkup tc39/spec.html tc39/shmem.html    
+fi
