b7a06c1f583452d5933e32d78e679a0fced7f590
Lars T Hansen <lth@acm.org>
2015-08-28 12:10:48 +0200

    Initial restatement of asm.js addenda

--- format.sh
+++ format.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# This script will create tc39/shmem.html from the spec sources.
+# This script will create tc39/shmem.html from the spec sources, and also
+# create asmjs/asmjs_shmem.html from the ditto sources.
 #
 # First, you need to:
 #
@@ -16,6 +17,8 @@
 
 if [ $(which iojs) != "" ]; then
     PATH=$(dirname $(which iojs)):$PATH ecmarkup tc39/spec.html tc39/shmem.html
+    PATH=$(dirname $(which iojs)):$PATH ecmarkup asmjs/asmjs_spec.html asmjs/asmjs_shmem.html
 else
     ecmarkup tc39/spec.html tc39/shmem.html    
+    ecmarkup asmjs_/spec.html asmjs/asmjs_shmem.html    
 fi
