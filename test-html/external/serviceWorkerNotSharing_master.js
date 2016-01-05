8f125e63d1f1c792596f46b772eb39d5e7514e18
Lars T Hansen <lth@acm.org>
2016-01-05 11:33:29 +0100

    WIP

--- test-html/external/serviceWorkerNotSharing_master.js
+++ test-html/external/serviceWorkerNotSharing_master.js
@@ -1,17 +1,39 @@
 if (!this.SharedArrayBuffer)
-    msg("No SharedArrayBuffer");
+    msg("Can't run: No SharedArrayBuffer");
 else if (!navigator.serviceWorker)
-    msg("No ServiceWorker");
+    msg("Can't run: No ServiceWorker");
 else
     runTest();
 
 function runTest() {
     var theWorker = null;
-
-    var sab = new SharedArrayBuffer(100);
+    var sab = new SharedArrayBuffer(236); // Non-random length
 
     navigator.serviceWorker.addEventListener('message', function (ev) {
-        console.log("Got a response: " + ev.data);
+	if (ev.data instanceof SharedArrayBuffer) {
+	    if (ev.data.byteLength == 200) {
+		msg("Master received a SharedArrayBuffer as expected");
+
+		// Modify the memory we just received so that the worker can check.
+		let ia2 = new Int32Array(ev.data);
+		ia2[12] = 0xdeadbeef;
+		ia2[33] = 0xcafebabe;
+	    }
+	    else
+		msg("ERROR: Master received a SharedArrayBuffer instance with unexpected length=" + ev.data.byteLength);
+
+	    // The worker believed it got a shared buffer, or we wouldn't be here.
+	    // See if the worker modified the shared memory the master sent.
+	    let ia = new Int32Array(sab);
+	    if (ia[37] != 0xdeadbeef || ia[42] != 0xcafebabe)
+		msg("ERROR: Memory not properly shared");
+	    else
+		msg("Worker's modifications are visible in the master as expected");
+	}
+	else if (typeof ev.data == "string")
+	    msg(ev.data);
+	else
+	    msg("ERROR: Master received unexpected data: " + ev.data);
     });
 
     navigator.serviceWorker.register('/ecmascript_sharedmem/tests/serviceWorkerNotSharing_worker.js',
@@ -25,18 +47,17 @@ function runTest() {
 
         if (theWorker) {
             setTimeout(function () {
-                console.log("Sending message now");
-                // This needs to fail, eventually.
-                theWorker.postMessage(sab, [sab]);
+		try {
+                    msg("Sending message from master to worker now");
+                    theWorker.postMessage(sab, [sab]);
+		    msg("Sending did not throw an error");
+		}
+		catch (e) {
+		    msg("EXCEPTION SEEN: postMessage from master to worker:\n" + e);
+		}
             }, 1000);
         }
     }).catch(function (error) {
-        console.log("Registration failed with " + error);
+        msg("Can't run: Registration failed with " + error);
     });
 }
-
-
-
-
-
-
--- test-html/external/serviceWorkerNotSharing_worker.js
+++ test-html/external/serviceWorkerNotSharing_worker.js
@@ -3,6 +3,32 @@ onfetch = function (ev) {
 }
 
 onmessage = function (ev) {
-    console.log("Got a message: " + ev.data);
-    console.log(ev.data instanceof SharedArrayBuffer);
+    if (ev.data instanceof SharedArrayBuffer) {
+	if (ev.data.byteLength = 236) {
+	    ev.source.postMessage("ServiceWorker received a SharedArrayBuffer as expected");
+	    let ia = new Int32Array(ev.data);
+	    ia[37] = 0xdeadbeef;
+	    ia[42] = 0xcafebabe;
+	}
+	else
+	    ev.source.postMessage("ERROR: ServiceWorker received a SharedArrayBuffer instance with unexpected length=" + ev.data.byteLength);
+    else
+	ev.source.postMessage("ERROR: ServiceWorker received unexpected data: " + ev.data);
+
+    var sab = new SharedArrayBuffer(200);
+    setTimeout(function () {
+	try {
+	    ev.source.postMessage("Will send SAB of length 200 from worker to master");
+	    ev.source.postMessage(sab, [sab]);
+	    ev.source.postMessage("Did send SAB from worker to master without exception");
+	    setTimeout(function () {
+		let ia = new Int32Array(sab);
+		if (ia[12] != 0xdeadbeef || ia[33] != 0xcafebabe)
+		    ev.source.postMessage("ERROR: The master did not properly update the worker's shared memory");
+		else
+		    ev.source.postMessage("Master's modifications are visible in the worker as expected");
+	    }, 1000);
+	} catch (e) {
+	    ev.source.postMessage("EXCEPTION SEEN: postMessage from worker to master: " + e);
+    }, 1000);
 }
