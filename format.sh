595a086ba991f2eb042ce02a2b687fe7b6fa3c70
Lars T Hansen <lth@acm.org>
2016-02-26 15:08:37 +0100

    Editorial changes to spec

--- format.sh
+++ format.sh
@@ -3,22 +3,12 @@
 # This script will create tc39/shmem.html from the spec sources, and also
 # create asmjs/asmjs_shmem.html from the ditto sources.
 #
-# First, you need to:
-#
-# Install io.js, because ecmarkup needs it:
-#   Download installer from iojs.org and run it
+# You need to:
 #
 # Install ecmarkup:
 #   sudo npm install -g ecmarkup
 #
 # ecmarkup 1.6 or greater is required.
 
-# Maybe io.js is not the default node, work around that if we can.
-
-if [ -n "$(which iojs)" ]; then
-    PATH=$(dirname $(which iojs)):$PATH ecmarkup tc39/spec.html tc39/shmem.html
-    PATH=$(dirname $(which iojs)):$PATH ecmarkup asmjs/asmjs_spec.html asmjs/asmjs_shmem.html
-else
-    ecmarkup tc39/spec.html tc39/shmem.html    
-    ecmarkup asmjs_/spec.html asmjs/asmjs_shmem.html    
-fi
+ecmarkup tc39/spec.html tc39/shmem.html    
+ecmarkup asmjs/asmjs_spec.html asmjs/asmjs_shmem.html    
